eab62822969ac16aef432b81fa5758a7d6135349
tuan50023072 <tuanvlapag@gmail.com>
2025-10-25 05:14:09 +0700

    cai dat xu li loi

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const router = require('./routes/routes')
 const app = express();
+const ApiError = require("./utils/apiError")
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +12,13 @@ app.get('/', (req, res) => {
   res.send('Server hoạt động bình thường ✅');
 });
 
-// Gắn tất cả routes
+app.use((req,res,next) => {
+  return next(new ApiError(404, " Resource not found "))
+});
 
+app.use((err,req,res, next) =>{
+  return res.status(err.statusCode || 500).json({
+    message : err.message || "Internal server error"
+  }) 
+});
 module.exports = app;
